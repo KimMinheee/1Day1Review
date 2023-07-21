22f5f95d6a97fed1aca05fee5f8046b7359fed63
KimMinheee <99alsgmlalsl@naver.com>
2023-07-21 16:56:07 +0900

    프로젝트 세팅

---
+++ src/main/java/com/programmers/OneDayOneReview/OneDayOneReviewApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.OneDayOneReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDayOneReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneDayOneReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programmers/OneDayOneReview/OneDayOneReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.OneDayOneReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneDayOneReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
