64d050219dcffaf36b822d073f4840c4e1c5058f
KimMinheee <99alsgmlalsl@naver.com>
2023-07-24 14:34:57 +0900

    [Feat] : BaseEntity 시간 반영 위한 Configuration 파일

---
+++ src/main/java/com/programmers/OneDayOneReview/global/common/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.programmers.OneDayOneReview.global.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
